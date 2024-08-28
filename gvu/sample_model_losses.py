3811977098b272bdb1f0f1384ed2bdedbc1e2ef5
patellae <62590607+arqamrp@users.noreply.github.com>
2024-08-28 17:49:55 +0100

    =indents

--- gvu/sample_model_losses.py
+++ gvu/sample_model_losses.py
@@ -9,57 +9,47 @@ def sample_model_losses(model, x, target, samples, device,
                         prior_loss_weight = 0,
                         div_type = None, alpha = None):
         
+    if rkl:
+        pass
+        # model.forward(frozen_prior = True)
+        # posterior_log_prob = model.posterior_log_probs()
+    
+    target_dims = model.layers[-1].out_features
+    batch_size = x.shape[0]
+    
+    preds = torch.zeros(samples, batch_size, target_dims).to(device)
+    for i in range(samples):
+        preds[i] = model.forward(x, sample = True)
 
-        if rkl:
-            pass
-            # model.forward(frozen_prior = True)
-            # posterior_log_prob = model.posterior_log_probs()
+    if unlearn and adj_lam != 0:
+        adjust = torch.zeros(samples, batch_size).to(device)
+        threshold = math.log(adj_lam) * model.max_log_prob_frozen_prior() 
+
+        if model.posterior_log_prob() > threshold:
+            adjust[i, :] += 1
+    else:
+        adjust = 1
+
+    
+
+    # compute negative log likelihood
+    if classification:
+        pred_probs = F.softmax(preds, dim = 1)      # probabilities from unnormalised logits
+        pred_probs_mean = pred_probs.mean(0)        # mean of probabilities across samples
+        target_probs = F.one_hot(target, num_classes= target_dims)
+        negative_log_likelihood = (-torch.sum(target_probs * torch.log(pred_probs_mean), dim=-1) * adjust ).mean()
+    else:
+        pred_mean = preds.mean(0)
+        negative_log_likelihood = (torch.sum( (pred_mean - target)**2, dim = -1) * adjust ).mean()
+
+    # holding tensors
+    if unlearn:
+        # prior loss
+        if prior_loss_weight != 0:
+            prior_preds = torch.zeros(samples, batch_size, target_dims).to(device)
+            for i in range(samples):
+                prior_preds[i] = model.forward(x, prior = True)
         
-
-
-
-        target_dims = model.layers[-1].out_features
-        batch_size = x.shape[0]
-        
-        # holding tensors
-        if unlearn:
-            
-            # prior loss
-            if prior_loss_weight != 0:
-                prior_preds = torch.zeros(samples, batch_size, target_dims).to(device)
-                for i in range(samples):
-                    prior_preds[i] = model.forward(x, prior = True)
-            
-            if adj_lam != 0:
-                adjust = torch.zeros(samples, batch_size).to(device)
-                threshold = math.log(adj_lam) * model.max_log_prob_frozen_prior() 
-
-                if model.posterior_log_prob() > threshold:
-                    adjust[i, :] += 1
-            else:
-                adjust = 1
-
-        else:
-            adjust = 1
-
-        preds = torch.zeros(samples, batch_size, target_dims).to(device)
-        for i in range(samples):
-            preds[i] = model.forward(x, sample = True)
-
-        # compute negative log likelihood
-        if classification:
-            pred_probs = F.softmax(preds, dim = 1)      # probabilities from unnormalised logits
-            pred_probs_mean = pred_probs.mean(0)        # mean of probabilities across samples
-            target_probs = F.one_hot(target, num_classes= target_dims)
-            negative_log_likelihood = (-torch.sum(target_probs * torch.log(pred_probs_mean), dim=-1) * adjust ).mean()
-        else:
-            pred_mean = preds.mean(0)
-            negative_log_likelihood = (torch.sum( (pred_mean - target)**2, dim = -1) * adjust ).mean()
-
-        
-        # compute prior predictive loss
-        
-        if prior_loss_weight != 0 and unlearn:
             if classification:
                 prior_pred_probs = F.softmax(prior_preds, dim = 1) # probabilities from unnormalised logits
                 prior_pred_probs_mean = prior_pred_probs.mean(0) # mean across samples
@@ -84,20 +74,19 @@ def sample_model_losses(model, x, target, samples, device,
         else:
             prior_pred_mean_loss = None
             # prior_pred_cov_loss = None
-        
-        divergence = model.divergence(unlearn = unlearn, div_type = div_type, alpha = alpha)
-        
-        
 
-        results = {
-            'prior_regularisation_term' : divergence,
-            'negative_log_likelihood': negative_log_likelihood,
-            'prior_pred_mean_loss' : prior_pred_mean_loss,
-        }
+    divergence = model.divergence(unlearn = unlearn, div_type = div_type, alpha = alpha)
 
-        if any(torch.isnan(results[key] if results[key] is not None else torch.zeros(1) ).any() for key in results):
+    
 
-            print('NaN detected in results')
-            print(results)
-        
-        return results
+    results = {
+        'prior_regularisation_term' : divergence,
+        'negative_log_likelihood': negative_log_likelihood,
+        'prior_pred_mean_loss' : prior_pred_mean_loss,
+    }
+
+    if any(torch.isnan(results[key] if results[key] is not None else torch.zeros(1) ).any() for key in results):
+        print('NaN detected in results')
+        print(results)
+    
+    return results
